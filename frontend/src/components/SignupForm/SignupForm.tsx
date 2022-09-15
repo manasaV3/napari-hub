abd3065d8382fee60b8a48ab69425913328297ad
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2022-09-15 14:25:54 -0700

    Address design feedback

--- frontend/src/components/SignupForm/SignupForm.tsx
+++ frontend/src/components/SignupForm/SignupForm.tsx
@@ -116,7 +116,7 @@ export function SignupForm({ onSubmit, variant = 'default' }: Props) {
             variant="contained"
             disableElevation
             className={clsx(
-              'bg-hub-primary-400',
+              'bg-hub-primary-400 hover:bg-hub-primary-500 active:bg-hub-primary-600',
               // font & colors
               'text-sm font-semibold text-black',
               // sizing
